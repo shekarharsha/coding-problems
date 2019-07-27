b0d28442b3755f912087c5fc060454bbdb0a4357
Harsha Shekar <harshashekar.1987@gmail.com>
2019-07-27 13:54:12 +0530

    Update App.java

--- practice/src/main/java/com/shekar/App.java
+++ practice/src/main/java/com/shekar/App.java
@@ -1,7 +1,6 @@
 package com.shekar;
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Hello world!
@@ -23,6 +22,24 @@ public class App
         for (String s : stack) {
             System.out.println(s);
         }
+
+        System.out.println();
+
+        List<String> items = new ArrayList<>();
+        items.add("Harsha");
+        items.add("Anvitha");
+        items.add("Atharva");
+        items.add("Usha");
+        items.add("Shekar");
+
+
+        Collections.sort(items, (o1, o2) -> {
+            if (o1.compareTo(o2) < 1) return 1;
+            else if (o1.compareTo(o2) > 1) return -1;
+            return 0;
+        });
+
+        items.stream().forEach(System.out::println);
 
     }
 }
